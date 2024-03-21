224a2534af46392f83d032ba67361596318bfb60
okiogutu <okiogutu@gmail.com>
2024-03-21 06:44:54 -0700

    modulus

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,37 +4,15 @@
 
 def minOperations(n):
     """calculates the least number of operations needed"""
-    pasted_chars = 1
-    clipboard = 0
-    counter = 0
-
-    while pasted chars < n:
-        if clipboard == 0:
-            clipboard = pasted chars
-            counter += 1
-
-        if pasted_chars == 1:
-            pasted_chars+= clipboard
-            counter += 1
-            continue
-
-    remaining = n - pasted_chars
-    # Remaining chars to paste
-    if remaining < clipboard:
+    if not isinstance(n, int):
         return 0
 
-    if remaining % pasted_chars != 0:
-        pasted_chars += clipboard
-        counter += 1
-    else:
-        clipboard = pasted_chars
-        pasted_chars += clipboard
-        counter += 2
-
-
-    if pasted_chars == n:
-        return counter
-    else:
-        return 0
-
-
+    operation = 0
+    iterator = 2
+    while (iterator <= n):
+        if not (n % iterator):
+            n = int(n / iterator)
+            operations += iterator
+            iterator = 1
+        iterator += 1
+    return operations
