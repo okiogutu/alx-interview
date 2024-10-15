50d524df28a0872b9681c4b6e00fe58e57853282
okiogutu <okiogutu@gmail.com>
2024-10-15 00:54:12 -0700

    review 5

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-''' Determine the fewest number of coins needed to meet
+'''Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet
     a given amount total.
 '''
 import sys
@@ -9,6 +10,7 @@ def makeChange(coins, total):
     '''
     Return: fewest number of coins needed to meet total
     If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
     '''
     if total <= 0:
         return 0
